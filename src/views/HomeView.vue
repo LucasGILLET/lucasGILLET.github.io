f34ea929c48b7f6d4220c0a8ebd4c00e1de73861
LucasPmep <lucas.gillet@pme-partner.fr>
2023-07-22 23:18:33 +0200

    add some visual changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-slate-200 px-[10rem] h-[40rem]">
-    <h1 class="text-3xl font-bold underline text-red-500 mx-auto">
+  <div class="bg-slate-200 px-[10rem] h-[40rem] dark:bg-gray-600 py-8">
+    <h1 class="text-3xl font-bold mx-auto text-center text-white">
       Bienvenue
     </h1>
   </div>
