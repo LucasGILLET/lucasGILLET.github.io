7023b08dd642d47f64dc8b50b6f13c172a460d0d
LucasPmep <lucas.gillet@pme-partner.fr>
2023-07-22 21:48:10 +0200

    fix bug github actions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="bg-slate-200 px-[10rem] h-[40rem]">
-    <h1 class="text-3xl font-bold underline text-red-500">
+    <h1 class="text-3xl font-bold underline text-red-500 mx-auto">
+      Bienvenue
     </h1>
   </div>
 </template>
